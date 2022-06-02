aed8c503cbb7564a102e80f32f38838f257f31c8
drvita <chavagalindo@live.com.mx>
2022-06-01 20:01:01 -0500

    Entries create form

--- src/pages/StoreEntries.jsx
+++ src/pages/StoreEntries.jsx
@@ -1,7 +1,34 @@
+import FormEntries from "../components/Store/FormEntries";
+
 export default function StoreEntries() {
   return (
-    <div>
-      <h1>Entries page</h1>
+    <div className="row">
+      <div className="col-12">
+        <div className="card card-primary card-outline">
+          <div className="card-header">
+            <h5 className="card-title mt-2 ml-2 text-bold">
+              Entradas de productos por lote
+            </h5>
+            <div className="card-tools">
+              <button
+                className="btn btn-small btn-primary"
+                title="Agregar una linea nueva"
+                alt="Agregar una linea nueva"
+              >
+                <i className="fas fa-plus" />
+              </button>
+            </div>
+          </div>
+
+          <div className="card-body">
+            <FormEntries />
+            <FormEntries />
+          </div>
+          <div className="card-footer text-right">
+            <button className="btn btn-primary">Enviar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

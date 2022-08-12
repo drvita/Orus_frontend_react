22815da895812d46f6970e3c87ed066b48e0780e
vmagana3 <gerzainnb@gmail.com>
2022-08-12 10:21:05 -0500

    ExamModal change width

--- src/components/Order/ExamModal.jsx
+++ src/components/Order/ExamModal.jsx
@@ -3,10 +3,11 @@ import Exam from '../Exam/views/examShort';
 
 export default function ExamModal({ handleClose: _close, exam }) {
   return (
-      <div className="modal d-block">
-      <div className="modal-dialog modal-lg" role="document">
+    <div className="modal d-block">
+      <div className="modal-dialog modal-lg" style={{maxWidth:'80%'}} role="document">
 
         <div className="modal-content">
+          
           <div className="modal-header">
             <h5 className="modal-title">Examen</h5>
             <button type="button" className="close" onClick={() => _close()}>
@@ -14,9 +15,7 @@ export default function ExamModal({ handleClose: _close, exam }) {
             </button>
           </div>
 
-
           <div className="modal-body p-2">
-
             <div className="row mt-4 d-print-none">
             {exam && exam.id ? (
               <div className="col pt-6">                
@@ -28,18 +27,7 @@ export default function ExamModal({ handleClose: _close, exam }) {
                   </div>
                 </div>
               </div>
-            ) : null}
-
-            {/* <div className={`col-${exam && exam.id ? 3 : 12} pt-6`}>
-              <h6 className="w-100 d-block">Meta data</h6>
-              <Dashboard
-                created_at={created_at ?? ""}
-                created={created ? created.name : ""}
-                updated={updated ? updated.name : ""}
-                updated_at={updated_at ?? ""}
-              />
-            </div> */}
-
+            ) : null}            
             </div>
           </div>
 
@@ -52,8 +40,8 @@ export default function ExamModal({ handleClose: _close, exam }) {
               <i className="fas fa-ban mr-1"></i>
               Cancelar
             </button>
-
           </div>
+
         </div>
       </div>
     </div> 
